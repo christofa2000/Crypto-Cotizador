16d0b86f4976a3b8f526f01892b0f89ffb81422b
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-12 17:14:27 -0600

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CriptoSearchForm from "./components/CriptoSearchForm"
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
           </h1>
 
           <div className="content">
-              
+              <CriptoSearchForm />
           </div>
       </div>
     </>
